aa6e44c8b281a6c2b0e7d995e107185d2a17b277
rldhont <rldhont@gmail.com>
2025-04-09 12:14:36 +0200

    Fix DBClient: remove schema

--- cartads/lib/dbClient.php
+++ cartads/lib/dbClient.php
@@ -21,9 +21,9 @@ class dbClient {
             round(ST_Area(p.geom)::numeric) AS surface,
             round(ST_Area(ST_Intersection(p.geom, ST_UNION(gz.geom)))::numeric) AS contenance,
             round((ST_Area(ST_Intersection(p.geom, ST_UNION(gz.geom))) / ST_Area(p.geom) * 100)::numeric, 2) AS occupation
-            FROM cartads.cartads_parcelle p
-            JOIN cartads.geo_zones gz ON (ST_INTERSECTS(p.geom, gz.geom) AND NOT ST_Touches(p.geom, gz.geom))
-            JOIN cartads.communes com ON (com.codeinsee=gz.codeinsee AND ST_CENTROID(p.geom) && com.geom AND ST_CONTAINS(com.geom, ST_CENTROID(p.geom)))
+            FROM cartads_parcelle p
+            JOIN geo_zones gz ON (ST_INTERSECTS(p.geom, gz.geom) AND NOT ST_Touches(p.geom, gz.geom))
+            JOIN communes com ON (com.codeinsee=gz.codeinsee AND ST_CENTROID(p.geom) && com.geom AND ST_CONTAINS(com.geom, ST_CENTROID(p.geom)))
             WHERE cartads_parcelle IN (' . implode(', ', array_map($quote, $parcelles)) . ')
             GROUP BY cartads_parcelle_id, p.cartads_parcelle, p.geom
         ';
@@ -39,10 +39,10 @@ class dbClient {
             zones_type AS type_nom,
             zones_type_code AS type_code,
             zones_observation AS observation
-            FROM cartads.zones z
-            JOIN cartads.geo_zones gz ON z.zones_id=gz.zones_id
-            JOIN cartads.cartads_parcelle p ON (ST_INTERSECTS(p.geom, gz.geom) AND NOT ST_Touches(p.geom, gz.geom))
-            JOIN cartads.communes com ON (com.codeinsee=gz.codeinsee AND ST_CENTROID(p.geom) && com.geom AND ST_CONTAINS(com.geom, ST_CENTROID(p.geom)))
+            FROM zones z
+            JOIN geo_zones gz ON z.zones_id=gz.zones_id
+            JOIN cartads_parcelle p ON (ST_INTERSECTS(p.geom, gz.geom) AND NOT ST_Touches(p.geom, gz.geom))
+            JOIN communes com ON (com.codeinsee=gz.codeinsee AND ST_CENTROID(p.geom) && com.geom AND ST_CONTAINS(com.geom, ST_CENTROID(p.geom)))
             WHERE p.cartads_parcelle IN (' . implode(', ', array_map($quote, $parcelles)) . ')
             ORDER BY pourcentage DESC
         ';
